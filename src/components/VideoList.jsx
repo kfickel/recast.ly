498842c02955d4f0df0afe47b3ef232e4a91c9de
Hack Reactor Student <student@hackreactor.com>
2017-10-25 16:46:06 -0700

    Got most of our views working

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,14 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+var VideoList = (props) => (
+  <div className="video-list"> 
+    {console.log(props)},
+    {props.videos.map((video, index) => 
+      <VideoListEntry key={index}
+                      video={video} />
+    )}
   </div>
 );
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 // TODO: Render the `App` component to the DOM
+//variable app 
+//console.log('Hi');
+
+// new VideoList(exampleVideoData);
+var myYoutubeData = window.exampleVideoData;
+console.log(myYoutubeData);
+
+
+ReactDOM.render(<App videos={window.exampleVideoData} />, document.getElementById('app'));
 
